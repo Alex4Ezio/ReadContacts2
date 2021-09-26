3e9426ddfab185400947b8a5b5dc2684e61da0bb
Alex4Ezio <1198985811@qq.com>
2021-09-26 16:52:05 +0800

    Initial commit ReadContactsList2

---
+++ app/src/main/java/cn/edu/bistu/readcontacts/MainActivity.java
@@ -0,0 +1,91 @@
+package cn.edu.bistu.readcontacts;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.ComponentInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.NetworkRequest;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.CaptioningManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    ArrayAdapter<String> adapter;
+    List<String> contactList = new ArrayList<>();
+    private ListView ListView_ReadContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.Button);
+        ListView_ReadContact = (ListView) findViewById(R.id.ListView_ReadContact);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactList);
+        ListView_ReadContact.setAdapter(adapter);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_CONTACTS}, 1);
+                } else {
+                    readContact();
+                }
+            }
+        });
+    }
+
+    public void readContact() {
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    //获取联系人姓名
+                    String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String Num=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contactList.add(displayName+"\n"+Num);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (cursor!=null){
+                cursor.close();
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    readContact();
+                }else {
+                    Toast.makeText(this,"未获取权限",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
